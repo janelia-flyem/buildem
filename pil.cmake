6fcacf16d598e2dff87825192aaba7dded891621
Bill Katz <billkatz@gmail.com>
2012-10-13 07:29:19 -0400

    Add PIL (python imaging library)

---
+++ pil.cmake
@@ -0,0 +1,41 @@
+#
+# Install PIL (python imaging library) from source
+#
+
+if (NOT pil_NAME)
+
+CMAKE_MINIMUM_REQUIRED(VERSION 2.8)
+
+include (ExternalProject)
+include (ExternalSource)
+include (BuildSupport)
+
+include (python)
+include (zlib)
+include (libjpeg)
+include (libpng)
+
+external_source (pil
+    1.17
+    Imaging-1.1.7.tar.gz
+    fc14a54e1ce02a0225be8854bfba478e
+    http://effbot.org/downloads)
+
+
+# Download and install pil
+message ("Installing ${pil_NAME} into FlyEM build area: ${FLYEM_BUILD_DIR} ...")
+
+ExternalProject_Add(${pil_NAME}
+    DEPENDS             ${python_NAME} ${zlib_NAME} ${libjpeg_NAME} ${libpng_NAME}
+    PREFIX              ${FLYEM_BUILD_DIR}
+    URL                 ${pil_URL}
+    URL_MD5             ${pil_MD5}
+    UPDATE_COMMAND      ""
+    PATCH_COMMAND       ""
+    CONFIGURE_COMMAND   ""
+    BUILD_COMMAND       ${FLYEM_ENV_STRING} ${PYTHON_EXE} setup.py install --prefix=${FLYEM_BUILD_DIR}
+    BUILD_IN_SOURCE     1
+    INSTALL_COMMAND     ""
+)
+
+endif (NOT pil_NAME)
