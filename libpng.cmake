daed15f09d325f9fc9b1de395b13ea783576391c
Bill Katz <katzw@janelia.hhmi.org>
2012-10-19 12:08:13 -0400

    Use libpng 1.5 as default

--- libpng.cmake
+++ libpng.cmake
@@ -10,20 +10,19 @@ include (ExternalProject)
 include (ExternalSource)
 include (BuildSupport)
 
-# Note: 'libpng-1.5 removed direct access to png_struct and png_info' so
-# FlyEM code relies on libpng-1.4.
-
-#external_source (libpng
-#    1.5.13
-#    libpng-1.5.13.tar.gz
-#    9c5a584d4eb5fe40d0f1bc2090112c65
-#    http://downloads.sourceforge.net/project/libpng/libpng15/1.5.13)
+# Note: 'libpng-1.5 removed direct access to png_struct and png_info'.
 
 external_source (libpng
-    1.4.12
-    libpng-1.4.12.tar.gz
-    72e4447061af5b781cd4057c4d449d80
-    http://downloads.sourceforge.net/project/libpng/libpng14/1.4.12)
+    1.5.13
+    libpng-1.5.13.tar.gz
+    9c5a584d4eb5fe40d0f1bc2090112c65
+    http://downloads.sourceforge.net/project/libpng/libpng15/1.5.13)
+
+#external_source (libpng
+#   1.4.12
+#    libpng-1.4.12.tar.gz
+#    72e4447061af5b781cd4057c4d449d80
+#    http://downloads.sourceforge.net/project/libpng/libpng14/1.4.12)
 
 
 
