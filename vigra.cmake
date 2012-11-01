31290b66705ef0571baf98eda979821720a4008e
Bill Katz <katzw@janelia.hhmi.org>
2012-11-01 15:22:50 -0400

    Simply by removing :string in -D command-line variable settings and also remove CMAKE_FIND_ROOT_PATH and use CMAKE_PREFIX_PATH.

--- vigra.cmake
+++ vigra.cmake
@@ -39,10 +39,6 @@ external_source (vigra
 #    vigra-flyem-1.7.1.tar.gz
 #    7325a6fed78383807fd553fc5ee30190)
 
-# Note the number of forced -D CMake variable sets in the configure.
-# There is trouble making vigra find the libraries given priority in 
-# CMAKE_FIND_ROOT_PATH and even using DEPENDENCY_SEARCH_PREFIX.  
-# Therefore, the problem libraries are set specifically.
 message ("Installing ${vigra_NAME} into FlyEM build area: ${FLYEM_BUILD_DIR} ...")
 ExternalProject_Add(${vigra_NAME}
     DEPENDS             ${libjpeg_NAME} ${libtiff_NAME} ${libpng_NAME} ${openexr_NAME} ${libfftw_NAME}
@@ -54,26 +50,26 @@ ExternalProject_Add(${vigra_NAME}
     PATCH_COMMAND       ""
     LIST_SEPARATOR      ^^
     CONFIGURE_COMMAND   ${FLYEM_ENV_STRING} ${CMAKE_COMMAND} ${vigra_SRC_DIR} 
-        -DCMAKE_INSTALL_PREFIX:string=${FLYEM_BUILD_DIR}
-        -DCMAKE_FIND_ROOT_PATH:string=${FLYEM_BUILD_DIR}
-        -DCMAKE_EXE_LINKER_FLAGS:string=${FLYEM_LDFLAGS}
-        -DDEPENDENCY_SEARCH_PREFIX:string=${FLYEM_BUILD_DIR}
-        -DBoost_LIBRARY_DIRS:string=${FLYEM_BUILD_DIR}/lib
-        -DBoost_PYTHON_LIBRARY:string=${FLYEM_BUILD_DIR}/lib/libboost_python.so
-        -DBoost_PYTHON_LIBRARY:string_RELEASE=${FLYEM_BUILD_DIR}/lib/libboost_python.so
-        -DBoost_PYTHON_LIBRARY:string_DEBUG=${FLYEM_BUILD_DIR}/lib/libboost_python.so
-        -DVIGRANUMPY_LIBRARIES:string=${FLYEM_BUILD_DIR}/lib/libpython2.7.so^^${FLYEM_BUILD_DIR}/lib/libboost_python.so
-        -DJPEG_INCLUDE_DIR:string=${FLYEM_BUILD_DIR}/include
-        -DJPEG_LIBRARY:string=${FLYEM_BUILD_DIR}/lib/libjpeg.so
-        -DHDF5_CORE_LIBRARY:string=${FLYEM_BUILD_DIR}/lib/libhdf5.so
-        -DHDF5_HL_LIBRARY:string=${FLYEM_BUILD_DIR}/lib/libhdf5_hl.so
-        -DHDF5_INCLUDE_DIR:string=${FLYEM_BUILD_DIR}/include
+        -DCMAKE_INSTALL_PREFIX=${FLYEM_BUILD_DIR}
+        -DCMAKE_PREFIX_PATH=${FLYEM_BUILD_DIR}
+        -DCMAKE_EXE_LINKER_FLAGS=${FLYEM_LDFLAGS}
+        -DDEPENDENCY_SEARCH_PREFIX=${FLYEM_BUILD_DIR}
+        -DBoost_LIBRARY_DIRS=${FLYEM_BUILD_DIR}/lib
+ #       -DBoost_PYTHON_LIBRARY=${FLYEM_BUILD_DIR}/lib/libboost_python-mt.so
+ #       -DBoost_PYTHON_LIBRARY_RELEASE=${FLYEM_BUILD_DIR}/lib/libboost_python-mt.so
+ #       -DBoost_PYTHON_LIBRARY_DEBUG=${FLYEM_BUILD_DIR}/lib/libboost_python-mt.so
+ #       -DVIGRANUMPY_LIBRARIES=${FLYEM_BUILD_DIR}/lib/libpython2.7.so^^${FLYEM_BUILD_DIR}/lib/libboost_python.so
+        -DJPEG_INCLUDE_DIR=${FLYEM_BUILD_DIR}/include
+        -DJPEG_LIBRARY=${FLYEM_BUILD_DIR}/lib/libjpeg.so
+        -DHDF5_CORE_LIBRARY=${FLYEM_BUILD_DIR}/lib/libhdf5.so
+        -DHDF5_HL_LIBRARY=${FLYEM_BUILD_DIR}/lib/libhdf5_hl.so
+        -DHDF5_INCLUDE_DIR=${FLYEM_BUILD_DIR}/include
         -DFFTW3F_INCLUDE_DIR=
         -DFFTW3F_LIBRARY=
-        -DFFTW3_INCLUDE_DIR:string=${FLYEM_BUILD_DIR}/include
-        -DFFTW3_LIBRARY:string=${FLYEM_BUILD_DIR}/lib/libfftw3.so
-        -DCMAKE_CXX_FLAGS:string=-pthread
-        -DCMAKE_CXX_LINK_FLAGS:string=-pthread
+        -DFFTW3_INCLUDE_DIR=${FLYEM_BUILD_DIR}/include
+        -DFFTW3_LIBRARY=${FLYEM_BUILD_DIR}/lib/libfftw3.so
+        -DCMAKE_CXX_FLAGS=-pthread
+        -DCMAKE_CXX_LINK_FLAGS=-pthread
     BUILD_COMMAND       ${FLYEM_ENV_STRING} make
     TEST_COMMAND        ${FLYEM_ENV_STRING} make check
     INSTALL_COMMAND     ${FLYEM_ENV_STRING} make install
