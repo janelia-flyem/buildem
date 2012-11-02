75a718af689060dbb6e18987f40b6fb4996db3b8
Bill Katz <katzw@janelia.hhmi.org>
2012-11-02 13:51:28 -0400

    Make sure numpy is built before scipy

--- scipy.cmake
+++ scipy.cmake
@@ -12,6 +12,7 @@ include (BuildSupport)
 include (FortranSupport)
 
 include (python)
+include (numpy)
 include (nose)
 include (atlas)
 
@@ -35,7 +36,7 @@ set (ENV{ATLAS} ${FLYEM_BUILD_DIR}/lib/libtatlas.so:${FLYEM_BUILD_DIR}/lib/libsa
 # Download and install scipy
 message ("Installing ${scipy_NAME} into FlyEM build area: ${FLYEM_BUILD_DIR} ...")
 ExternalProject_Add(${scipy_NAME}
-    DEPENDS             ${python_NAME} ${nose_NAME} ${atlas_NAME} 
+    DEPENDS             ${python_NAME} ${numpy_NAME} ${nose_NAME} ${atlas_NAME} 
     PREFIX              ${FLYEM_BUILD_DIR}
     URL                 ${scipy_URL}
     URL_MD5             ${scipy_MD5}
