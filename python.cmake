3bf28e38f69586f524498ac9519367c7f4a729c1
Stuart Berg <bergs@janelia.hhmi.org>
2012-11-09 16:44:56 -0500

    python.cmake: Support both mac and linux.

--- python.cmake
+++ python.cmake
@@ -23,6 +23,18 @@ external_source (python
 
 message ("Installing ${python_NAME} into FlyEM build area: ${FLYEM_BUILD_DIR} ...")
 
+if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+    # On Mac, we build a "Framework" build which has all the power of a "normal" build, 
+    #  plus it can be used from a native GUI.
+    # See http://svn.python.org/projects/python/trunk/Mac/README
+    set (PYTHON_BUILD_TYPE_ARG "--enable-framework=${FLYEM_BUILD_DIR}/Frameworks")
+    set (PYTHON_PREFIX ${FLYEM_BUILD_DIR}/Frameworks/Python.framework/Versions/2.7)
+else()
+    # On linux, PYTHON_PREFIX is the same as the general prefix.
+    set (PYTHON_BUILD_TYPE_ARG "--enable-shared")
+    set (PYTHON_PREFIX ${FLYEM_BUILD_DIR})
+endif()
+
 ExternalProject_Add(${python_NAME}
     DEPENDS             ${zlib_NAME}
     PREFIX              ${FLYEM_BUILD_DIR}
@@ -32,16 +44,15 @@ ExternalProject_Add(${python_NAME}
     PATCH_COMMAND       ""
     CONFIGURE_COMMAND   ${FLYEM_ENV_STRING} ${python_SRC_DIR}/configure 
         --prefix=${FLYEM_BUILD_DIR}
-        --enable-framework=${FLYEM_BUILD_DIR}/Frameworks
+        ${PYTHON_BUILD_TYPE_ARG}
         LDFLAGS=${FLYEM_LDFLAGS}
         CPPFLAGS=-I${FLYEM_BUILD_DIR}/include
     BUILD_COMMAND       ${FLYEM_ENV_STRING} make
     INSTALL_COMMAND     ${FLYEM_ENV_STRING} make install
-    BUILD_IN_SOURCE 1
+    BUILD_IN_SOURCE 1 # Required for Mac
 )
 
-set (PYTHON_PREFIX ${FLYEM_BUILD_DIR}/Frameworks/Python.framework/Versions/2.7)
-set (PYTHON_INCLUDE_PATH ${FLYEM_BUILD_DIR}/Frameworks/Python.framework/Versions/2.7/include/python2.7)
-set (PYTHON_EXE ${FLYEM_BUILD_DIR}/Frameworks/Python.framework/Versions/2.7/bin/python)
+set (PYTHON_INCLUDE_PATH ${PYTHON_PREFIX}/include/python2.7)
+set (PYTHON_EXE ${PYTHON_PREFIX}/bin/python)
 
 endif (NOT python_NAME)
