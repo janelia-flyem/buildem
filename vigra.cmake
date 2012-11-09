0514fa66711abe95aadfac4e499ad3c4af3089f9
Stuart Berg <bergs@janelia.hhmi.org>
2012-11-09 16:52:06 -0500

    vigra.cmake: Only apply patch for mac, and use variable to determine dylib extension (.dylib for mac, .so for linux)

--- vigra.cmake
+++ vigra.cmake
@@ -9,6 +9,7 @@ CMAKE_MINIMUM_REQUIRED(VERSION 2.8)
 include (ExternalProject)
 include (ExternalSource)
 include (BuildSupport)
+include (PatchSupport)
 
 include (libjpeg)
 include (libtiff)
@@ -45,6 +46,18 @@ external_source (vigra
 #    vigra-flyem-1.7.1.tar.gz
 #    7325a6fed78383807fd553fc5ee30190)
 
+if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+    # The vigra 1.9.0 release assumes all mac builds are "Framework" builds.
+    # This patch allows us to build vigra against non-Framework builds, too.
+    # NOTE: This issue will be fixed in vigra-HEAD on github soon, so this patch 
+    #       won't be necessary soon.  The patch may fail to apply when you upgrade
+    #       vigra beyond 1.9.0.
+    set (vigra_PATCH ${FLYEM_ENV_STRING} ${PATCH_EXE}
+        ${vigra_SRC_DIR}/config/FindVIGRANUMPY_DEPENDENCIES.cmake ${PATCH_DIR}/vigra.patch )
+else()
+    set (vigra_PATCH "")
+endif()
+
 message ("Installing ${vigra_NAME} into FlyEM build area: ${FLYEM_BUILD_DIR} ...")
 ExternalProject_Add(${vigra_NAME}
     DEPENDS             ${libjpeg_NAME} ${libtiff_NAME} ${libpng_NAME} ${openexr_NAME} ${libfftw_NAME}
@@ -53,8 +66,7 @@ ExternalProject_Add(${vigra_NAME}
     URL                 ${vigra_URL}
     URL_MD5             ${vigra_MD5}
     UPDATE_COMMAND      ""
-    PATCH_COMMAND       ${FLYEM_ENV_STRING} ${PATCH_EXE}
-        ${vigra_SRC_DIR}/config/FindVIGRANUMPY_DEPENDENCIES.cmake ${PATCH_DIR}/vigra.patch
+    PATCH_COMMAND       ${vigra_PATCH}       
     LIST_SEPARATOR      ^^
     CONFIGURE_COMMAND   ${FLYEM_ENV_STRING} ${CMAKE_COMMAND} ${vigra_SRC_DIR} 
         -DCMAKE_INSTALL_PREFIX=${FLYEM_BUILD_DIR}
@@ -62,19 +74,19 @@ ExternalProject_Add(${vigra_NAME}
         -DCMAKE_EXE_LINKER_FLAGS=${FLYEM_LDFLAGS}
         -DDEPENDENCY_SEARCH_PREFIX=${FLYEM_BUILD_DIR}
         -DBoost_LIBRARY_DIRS=${FLYEM_BUILD_DIR}/lib
- #       -DBoost_PYTHON_LIBRARY=${FLYEM_BUILD_DIR}/lib/libboost_python-mt.dylib
- #       -DBoost_PYTHON_LIBRARY_RELEASE=${FLYEM_BUILD_DIR}/lib/libboost_python-mt.dylib
- #       -DBoost_PYTHON_LIBRARY_DEBUG=${FLYEM_BUILD_DIR}/lib/libboost_python-mt.dylib
- #       -DVIGRANUMPY_LIBRARIES=${FLYEM_BUILD_DIR}/lib/libpython2.7.dylib^^${FLYEM_BUILD_DIR}/lib/libboost_python.dylib
+ #       -DBoost_PYTHON_LIBRARY=${FLYEM_BUILD_DIR}/lib/libboost_python-mt.${FLYEM_PLATFORM_DYLIB_EXTENSION}
+ #       -DBoost_PYTHON_LIBRARY_RELEASE=${FLYEM_BUILD_DIR}/lib/libboost_python-mt.${FLYEM_PLATFORM_DYLIB_EXTENSION}
+ #       -DBoost_PYTHON_LIBRARY_DEBUG=${FLYEM_BUILD_DIR}/lib/libboost_python-mt.${FLYEM_PLATFORM_DYLIB_EXTENSION}
+ #       -DVIGRANUMPY_LIBRARIES=${FLYEM_BUILD_DIR}/lib/libpython2.7.${FLYEM_PLATFORM_DYLIB_EXTENSION}^^${FLYEM_BUILD_DIR}/lib/libboost_python.${FLYEM_PLATFORM_DYLIB_EXTENSION}
         -DJPEG_INCLUDE_DIR=${FLYEM_BUILD_DIR}/include
-        -DJPEG_LIBRARY=${FLYEM_BUILD_DIR}/lib/libjpeg.dylib
-        -DHDF5_CORE_LIBRARY=${FLYEM_BUILD_DIR}/lib/libhdf5.dylib
-        -DHDF5_HL_LIBRARY=${FLYEM_BUILD_DIR}/lib/libhdf5_hl.dylib
+        -DJPEG_LIBRARY=${FLYEM_BUILD_DIR}/lib/libjpeg.${FLYEM_PLATFORM_DYLIB_EXTENSION}
+        -DHDF5_CORE_LIBRARY=${FLYEM_BUILD_DIR}/lib/libhdf5.${FLYEM_PLATFORM_DYLIB_EXTENSION}
+        -DHDF5_HL_LIBRARY=${FLYEM_BUILD_DIR}/lib/libhdf5_hl.${FLYEM_PLATFORM_DYLIB_EXTENSION}
         -DHDF5_INCLUDE_DIR=${FLYEM_BUILD_DIR}/include
         -DFFTW3F_INCLUDE_DIR=
         -DFFTW3F_LIBRARY=
         -DFFTW3_INCLUDE_DIR=${FLYEM_BUILD_DIR}/include
-        -DFFTW3_LIBRARY=${FLYEM_BUILD_DIR}/lib/libfftw3.dylib
+        -DFFTW3_LIBRARY=${FLYEM_BUILD_DIR}/lib/libfftw3.${FLYEM_PLATFORM_DYLIB_EXTENSION}
         -DCMAKE_CXX_FLAGS=-pthread
         -DCMAKE_CXX_LINK_FLAGS=-pthread
     BUILD_COMMAND       ${FLYEM_ENV_STRING} make
