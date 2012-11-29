4c9cf8101a400e2f9567b23e2472a1f2f883213a
Bill Katz <katzw@janelia.hhmi.org>
2012-11-29 15:05:09 -0500

    Add PyOpenGL -- required for Raveler quadtree lib

---
+++ pyopengl.cmake
@@ -0,0 +1,34 @@
+#
+# Install PyOpenGL from source
+#
+
+if (NOT pyopengl_NAME)
+
+CMAKE_MINIMUM_REQUIRED(VERSION 2.8)
+
+include (ExternalProject)
+include (ExternalSource)
+include (BuildSupport)
+
+include (python)
+
+external_source (pyopengl
+    3.0.2
+    PyOpenGL-3.0.2.tar.gz
+    77becc24ffc0a6b28030aa109ad7ff8b
+    http://pypi.python.org/packages/source/P/PyOpenGL/PyOpenGL-3.0.2.tar.gz)
+
+message ("Installing ${pyopengl_NAME} into FlyEM build area: ${BUILDEM_DIR} ...")
+ExternalProject_Add(${pyopengl_NAME}
+    DEPENDS             ${python_NAME}
+    PREFIX              ${BUILDEM_DIR}
+    URL                 ${pyopengl_URL}
+    URL_MD5             ${pyopengl_MD5}
+    UPDATE_COMMAND      ""
+    CONFIGURE_COMMAND   ""
+    BUILD_COMMAND       ${BUILDEM_ENV_STRING} ${PYTHON_EXE} setup.py install
+    BUILD_IN_SOURCE     1
+    INSTALL_COMMAND     ""
+)
+
+endif (NOT pyopengl_NAME)
