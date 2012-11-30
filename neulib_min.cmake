bf4299246d200ca3bf2a4736764377b652b73edd
Ting Zhao <tingzhao@gmail.com>
2012-11-30 09:40:54 -0500

    add lib name suffix

--- neulib_min.cmake
+++ neulib_min.cmake
@@ -6,7 +6,7 @@ if (NOT libneurolabi_NAME)
 
   include(${BUILDEM_REPO_DIR}/neulib_com.cmake)
 
-  SET(config_args --enable-shared --disable-fftw --disable-fftwf --disable-z --disable-xml --disable-png --disable-jansson) 
+  SET(config_args --enable-shared --disable-fftw --disable-fftwf --disable-z --disable-xml --disable-png --disable-jansson LIB_SUFFIX=_min) 
   INSTALL_NEULIB(libneurolabi_min "${config_args}")
 
 endif (NOT libneurolabi_NAME)
