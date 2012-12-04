5ecabcecb4e8043e16c326f6eac446927448d02d
Bill Katz <katzw@janelia.hhmi.org>
2012-12-04 13:32:12 -0500

    Add dependencies for emdata module

--- emdata.cmake
+++ emdata.cmake
@@ -4,13 +4,19 @@ include (ExternalProject)
 include (BuildSupport)
 include (ExternalSource)
 
+include (gtest)
+include (leveldb)
+include (thrift)
+include (python)
+include (setuptools)
+
 external_git_repo (emdata
     HEAD
     http://github.com/janelia-flyem/emdata.git)
 
 message ("Installing ${emdata_NAME} into FlyEM build area: ${BUILDEM_DIR} ...")
 ExternalProject_Add(${emdata_NAME}
-    DEPENDS             ${python_NAME} ${setuptools_NAME}
+    DEPENDS             ${gtest_NAME} ${leveldb_NAME} ${thrift_NAME} ${python_NAME} ${setuptools_NAME}
     PREFIX              ${BUILDEM_DIR}
     GIT_REPOSITORY      ${emdata_URL}
     UPDATE_COMMAND      ""
