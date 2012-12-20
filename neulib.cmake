99b415187267d10d0b036e8977e40b147de17f49
Ting Zhao <tingzhao@gmail.com>
2012-12-20 17:14:12 -0500

    neulib fix

--- neulib.cmake
+++ neulib.cmake
@@ -7,7 +7,7 @@ if (NOT libneurolabi_NAME)
   include(${BUILDEM_REPO_DIR}/neulib_com.cmake)
 
   SET(config_args --enable-shared) 
-  SET(depends_args jansson libfftw libxml2 libjansson libpng zlib) 
+  SET(depends_args libjansson libfftw libxml2 libjansson libpng zlib) 
   INSTALL_NEULIB(libneurolabi "${config_args}")
 
 endif (NOT libneurolabi_NAME)
--- neulib_jansson.cmake
+++ neulib_jansson.cmake
@@ -7,7 +7,7 @@ if (NOT libneurolabi_NAME)
   include(${BUILDEM_REPO_DIR}/neulib_com.cmake)
 
   SET(config_args --enable-shared --disable-fftw --disable-fftwf --disable-z --disable-xml --disable-png LIB_SUFFIX=_jansson) 
-  SET(depends_args jansson)
-  INSTALL_NEULIB(libneurolabi_min "${config_args}" "${depends_args}")
+  SET(depends_args libjansson)
+  INSTALL_NEULIB(libneurolabi_jansson "${config_args}" "${depends_args}")
 
 endif (NOT libneurolabi_NAME)
