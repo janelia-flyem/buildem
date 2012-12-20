1ee06c709c50683268b85b45534953839932145f
Ting Zhao <tingzhao@gmail.com>
2012-12-20 17:22:05 -0500

    libjansson fix

--- neulib_jansson.cmake
+++ neulib_jansson.cmake
@@ -7,7 +7,7 @@ if (NOT libneurolabi_NAME)
   include(${BUILDEM_REPO_DIR}/neulib_com.cmake)
 
   SET(config_args --enable-shared --disable-fftw --disable-fftwf --disable-z --disable-xml --disable-png LIB_SUFFIX=_jansson) 
-  SET(depends_args libjansson)
+  SET(depends_args ${libjansson_NAME})
   INSTALL_NEULIB(libneurolabi_jansson "${config_args}" "${depends_args}")
 
 endif (NOT libneurolabi_NAME)
