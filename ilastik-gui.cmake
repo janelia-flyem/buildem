e08ce3b4e56db0b57ca3558c891b30cdd7df7e93
Stuart Berg <bergs@janelia.hhmi.org>
2013-02-20 17:07:33 -0500

    ilastik-gui: Added chatty and cylemon as dependencies, and added carving workflow launch script. - cylemon is needed for the carving workflow - chatty isn't really needed for ilastik per se, but will probably be needed in future workflows.

--- ilastik-gui.cmake
+++ ilastik-gui.cmake
@@ -20,11 +20,13 @@ include (qt4)
 include (pyqt4)
 include (qimage2ndarray)
 include (vtk)
+include (chatty)
+include (cylemon)
 
 set (ilastik-gui_NAME ${ilastik_NAME}-gui)
 
 # Add a few dependencies to GUI ilastik build
-add_dependencies( ${ilastik_NAME} ${qt4_NAME} ${pyqt4_NAME} ${qimage2ndarray_NAME} ${vtk_NAME}) 
+add_dependencies( ${ilastik_NAME} ${qt4_NAME} ${pyqt4_NAME} ${qimage2ndarray_NAME} ${vtk_NAME} ${chatty_NAME} $(cylemon_NAME) ) 
 
 add_custom_target (${ilastik-gui_NAME} ALL 
     DEPENDS     ${ilastik_NAME}
@@ -65,6 +67,17 @@ ExternalProject_add_step(${ilastik_NAME}  install_gui_ws_launch # Alternate GUI 
         ${BUILDEM_DIR}/bin/setenv_ilastik_gui.sh
         ${ilastik_SRC_DIR}/ilastik/workflows/vigraWatershed/pixelClassificationWithWatershedMain.py
     COMMENT     "Added ilastik gui-ws command to bin directory"
+)
+
+ExternalProject_add_step(${ilastik_NAME}  install_gui_carving_launch # Alternate GUI: Carving workflow
+    DEPENDEES   install_gui_env_script
+    COMMAND     ${TEMPLATE_EXE}
+        --exe
+        ${TEMPLATE_DIR}/ilastik_script.template
+        ${BUILDEM_DIR}/bin/ilastik_gui_carving
+        ${BUILDEM_DIR}/bin/setenv_ilastik_gui.sh
+        ${ilastik_SRC_DIR}/ilastik/workflows/carving/carving.py
+    COMMENT     "Added ilastik gui-carving command to bin directory"
 )
 
 ExternalProject_add_step(${ilastik_NAME}  install_gui_test
