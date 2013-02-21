50c74ac915618e3318cd8672437d49ccc7a894b9
Stuart Berg <bergs@janelia.hhmi.org>
2013-02-21 08:30:25 -0500

    ilastik.cmake: Updated git tag.

--- ilastik.cmake
+++ ilastik.cmake
@@ -27,7 +27,7 @@ include (blist)
 include (greenlet)
 
 external_git_repo (ilastik
-    flyem-20130207B
+    flyem-20130221
     http://github.com/janelia-flyem/flyem-ilastik)
 
 message ("Installing ${ilastik_NAME} into FlyEM build area: ${BUILDEM_DIR} ...")
