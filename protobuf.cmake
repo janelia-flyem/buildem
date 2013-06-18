05e80f758d22f9d6caa07d0e6718cf6635da1bcc
Bill Katz <billkatz@gmail.com>
2013-06-18 11:06:15 -0400

    Fix leveldb unix install.  Add protobuf

---
+++ protobuf.cmake
@@ -0,0 +1,40 @@
+#
+# Install protobuf from source
+#
+
+if (NOT protobuf_NAME)
+
+CMAKE_MINIMUM_REQUIRED(VERSION 2.8)
+
+include (ExternalProject)
+include (ExternalSource)
+include (BuildSupport)
+
+external_source (protobuf
+    2.5.0
+    protobuf-2.5.0.tar.gz
+    b751f772bdeb2812a2a8e7202bf1dae8
+    https://protobuf.googlecode.com/files)
+
+message ("Installing ${protobuf_NAME} into build area: ${BUILDEM_DIR} ...")
+ExternalProject_Add(${protobuf_NAME}
+    PREFIX            ${BUILDEM_DIR}
+    URL               ${protobuf_URL}
+    URL_MD5           ${protobuf_MD5}
+    UPDATE_COMMAND    ""
+    PATCH_COMMAND     ""
+    CONFIGURE_COMMAND   ${BUILDEM_ENV_STRING} ./configure 
+        --prefix=${BUILDEM_DIR} 
+        --enable-shared
+        --enable-static
+        LDFLAGS=${BUILDEM_LDFLAGS}
+        CPPFLAGS=-I${BUILDEM_DIR}/include
+    BUILD_COMMAND     ${BUILDEM_ENV_STRING} make
+    BUILD_IN_SOURCE   1
+    TEST_COMMAND      ${BUILDEM_ENV_STRING} make check
+    INSTALL_COMMAND   ${BUILDEM_ENV_STRING} make install
+)
+
+set_target_properties(${protobuf_NAME} PROPERTIES EXCLUDE_FROM_ALL ON)
+
+endif (NOT protobuf_NAME)
