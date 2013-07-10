7dc3af27b314999979839cd255756278108913b0
Philipp Hanslovsky <Philipp.Hanslovsky@iwr.uni-heidelberg.de>
2013-07-10 12:10:59 +0200

    added ann

---
+++ ann.cmake
@@ -0,0 +1,42 @@
+#
+# Install ann from source
+#
+
+if (NOT ann_NAME)
+
+CMAKE_MINIMUM_REQUIRED(VERSION 2.8)
+
+include (ExternalProject)
+include (ExternalSource)
+include (BuildSupport)
+include (PatchSupport)
+
+external_source (ann
+    1.1.2
+    ann_1.1.2.tar.gz
+    7ffaacc7ea79ca39d4958a6378071365
+    http://www.cs.umd.edu/~mount/ANN/Files/1.1.2)
+
+message ("Installing ${ann_NAME} into FlyEM build area: ${BUILDEM_DIR} ...")
+ExternalProject_Add(${ann_NAME}
+    PREFIX              ${BUILDEM_DIR}
+    URL                 ${ann_URL}
+    URL_MD5             ${ann_MD5}
+    UPDATE_COMMAND      ""
+    PATCH_COMMAND       ""
+
+    CONFIGURE_COMMAND   cp ann_CMakeLists.txt ${ann_SRC_DIR} &&
+        ${BUILDEM_ENV_STRING} ${CMAKE_COMMAND} ${ann_SRC_DIR} 
+        -DBUILD_SHARED_LIBS=ON
+        -DCMAKE_INSTALL_PREFIX=${BUILDEM_DIR}
+        -DCMAKE_PREFIX_PATH=${BUILDEM_DIR}
+
+
+    BUILD_COMMAND       ${BUILDEM_ENV_STRING} make
+    INSTALL_COMMAND     ${BUILDEM_ENV_STRING} make install
+    TEST_COMMAND        ${BUILDEM_ENV_STRING} make check
+)
+
+set_target_properties(${ann_NAME} PROPERTIES EXCLUDE_FROM_ALL ON)
+
+endif (NOT ann_NAME)
