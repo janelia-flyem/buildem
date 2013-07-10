d97884405705addfa5dc73684102ab4b644d921b
Philipp Hanslovsky <Philipp.Hanslovsky@iwr.uni-heidelberg.de>
2013-07-10 17:28:31 +0200

    modified libann url and patch command

--- ann.cmake
+++ ann.cmake
@@ -15,7 +15,7 @@ external_source (libann
     1.1.2
     libann_1.1.2.tar.gz
     7ffaacc7ea79ca39d4958a6378071365
-    http://www.cs.umd.edu/~mount/LIBANN/Files/1.1.2)
+    http://www.cs.umd.edu/~mount/ANN/Files/1.1.2)
 
 message ("Installing ${libann_NAME} into FlyEM build area: ${BUILDEM_DIR} ...")
 ExternalProject_Add(${libann_NAME}
@@ -23,7 +23,8 @@ ExternalProject_Add(${libann_NAME}
     URL                 ${libann_URL}
     URL_MD5             ${libann_MD5}
     UPDATE_COMMAND      ""
-    PATCH_COMMAND       "cp patches/ann.patch ${libann_SRC_DIR}"
+    PATCH_COMMAND       ${BUILDEM_ENV_STRING} ${PATCH_EXE}
+    	${libann_SRC_DIR}/CMakeLists.txt ${PATCH_DIR}/ann.patch
 
     CONFIGURE_COMMAND   ${BUILDEM_ENV_STRING} ${CMAKE_COMMAND} ${libann_SRC_DIR} 
         -DBUILD_SHARED_LIBS=ON
@@ -39,3 +40,4 @@ ExternalProject_Add(${libann_NAME}
 set_target_properties(${libann_NAME} PROPERTIES EXCLUDE_FROM_ALL ON)
 
 endif (NOT libann_NAME)
+
