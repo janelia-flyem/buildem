8c7dc3fd99fe41310fe0f74b421d324da9683534
Philipp Hanslovsky <philipp.hanslovsky@gmail.com>
2013-07-17 17:36:33 +0200

    removed check target for ann

--- ann.cmake
+++ ann.cmake
@@ -35,7 +35,7 @@ ExternalProject_Add(${ann_NAME}
 
     BUILD_COMMAND       ${BUILDEM_ENV_STRING} make
     INSTALL_COMMAND     ${BUILDEM_ENV_STRING} make install
-    TEST_COMMAND        ${BUILDEM_ENV_STRING} make check
+    TEST_COMMAND        "" # ${BUILDEM_ENV_STRING} make check
 )
 
 set_target_properties(${ann_NAME} PROPERTIES EXCLUDE_FROM_ALL ON)
