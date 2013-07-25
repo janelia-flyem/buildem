529f77c10d657cfb38ede09b6eb9558f41f39905
Buote Xu <Buote.Xu@gmail.com>
2013-07-25 18:59:38 +0200

    added counting

---
+++ counting.cmake
@@ -0,0 +1,57 @@
+#
+# Install ilastik headless (non-GUI) from source
+#
+# Ilastik is composed of 3 git repos, 2 of which are necessary for headless mode.
+#
+# lazyflow
+# applet-workflows
+# volumina (for gui builds)
+#
+# Also, you must build lazyflow/lazyflow/drtile with CMake to produce drtile.so shared library.
+# This is done in the CONFIGURE_COMMAND below and the shared library is saved in the
+# drtile source directory.
+
+if (NOT counting_NAME)
+
+CMAKE_MINIMUM_REQUIRED(VERSION 2.8)
+
+include (ExternalProject)
+include (ExternalSource)
+include (BuildSupport)
+include (TemplateSupport)
+
+include(ilastik)
+
+message ("Installing ${counting_NAME} into FlyEM build area: ${BUILDEM_DIR} ...")
+if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+    # On Mac OS X, building drtile requires explicitly setting several cmake cache variables
+    ExternalProject_Add(${ilastik_NAME}
+	DEPENDS             ${ilastik_NAME}
+
+	PREFIX              ${BUILDEM_DIR}
+        GIT_REPOSITORY      ${ilastik_URL}
+        UPDATE_COMMAND      ""
+        PATCH_COMMAND       ""
+        CONFIGURE_COMMAND   ""
+        BUILD_COMMAND       ""
+        TEST_COMMAND        ${BUILDEM_DIR}/bin/ilastik_headless_test
+        INSTALL_COMMAND     ""
+    )
+else()
+    # On Linux, building drtile requires less explicit configuration
+    # The explicit configuration above would probably work, but let's keep this simple...
+    ExternalProject_Add(${counting_NAME}
+	DEPENDS             ${ilastik_NAME} 
+        PREFIX              ${BUILDEM_DIR}
+        GIT_REPOSITORY      ${ilastik_URL}
+        UPDATE_COMMAND      ""
+        PATCH_COMMAND       ""
+	CONFIGURE_COMMAND   ${BUILDEM_ENV_STRING} ${CMAKE_COMMAND} 
+		-DCMAKE_PREFIX_PATH=${BUILDEM_DIR} 
+		-DLIBRARY_OUTPUT_PATH=${ilastik_SRC_DIR}/ilastik/applets/counting/cplexwrapper 
+		${ilastik_SRC_DIR}/ilastik/applets/counting/cplexwrapper
+	BUILD_COMMAND       ${BUILDEM_ENV_STRING} make
+        TEST_COMMAND        ""
+        INSTALL_COMMAND     ""
+    )
+endif()
