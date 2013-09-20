8b8bd5327c32ccbb782874e0e70b4c10f3f91038
Stuart Berg <bergs@janelia.hhmi.org>
2013-09-20 13:11:01 -0400

    blas.cmake: Emit a warning to indicate that OpenBLAS doesn't compile correctly on Mac.

--- blas.cmake
+++ blas.cmake
@@ -7,11 +7,19 @@ SET(WITH_ATLAS ${WITH_ATLAS}
     CACHE BOOL "Use ATLAS (WITH_ATLAS=1) or openBLAS (WITH_ATLAS=0) ?"
     FORCE)
 
-
 if(WITH_ATLAS)
     include (atlas)
     set(blas_NAME ${atlas_NAME})
 else()
+    # On Mac, warn that openblas might not work.
+    if (${APPLE})
+        # Only emit this warning once.
+        if ("${emitted_warning_for_openblas_osx}" STREQUAL "")
+            message(WARNING "\n*** OpenBLAS does not always successfully build on Mac OS X.  Consider using -DWITH_ATLAS=1 instead. ***\n")
+            set (emitted_warning_for_openblas_osx 1)
+        endif ()
+    endif ()
+
     include (openblas)
     set(blas_NAME ${openblas_NAME})
 endif()
