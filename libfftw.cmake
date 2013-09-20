b487067bf530b941abb220d9b93d01f2172f5f1c
Stuart Berg <bergs@janelia.hhmi.org>
2013-09-20 12:07:12 -0400

    libfftw.cmake: Generate BOTH double and single-precision float libraries.

--- libfftw.cmake
+++ libfftw.cmake
@@ -26,11 +26,36 @@ ExternalProject_Add(${libfftw_NAME}
     CONFIGURE_COMMAND   ${BUILDEM_ENV_STRING} ${libfftw_SRC_DIR}/configure 
         --prefix=${BUILDEM_DIR}
         --enable-shared
-        --enable-float  # This creates libfftw3f single-precision libraries
         LDFLAGS=${BUILDEM_LDFLAGS}
         CPPFLAGS=-I${BUILDEM_DIR}/include
     BUILD_COMMAND       ${BUILDEM_ENV_STRING} make
     INSTALL_COMMAND     ${BUILDEM_ENV_STRING} make install
+)
+
+# Add additional steps to build and install the fftw single-precision libraries.
+# (It is not possible to generate single and double precision libraries in one make step.)
+
+# Configure single-precision
+ExternalProject_Add_Step(${libfftw_NAME} singlefloat-configure
+    DEPENDEES install
+    COMMAND cd ${libfftw_SRC_DIR}-build && ${BUILDEM_ENV_STRING} ${libfftw_SRC_DIR}/configure 
+        --prefix=${BUILDEM_DIR}
+        --enable-shared
+        --enable-float  # This creates libfftw3f single-precision libraries INSTEAD OF the default double libraries.
+        LDFLAGS=${BUILDEM_LDFLAGS}
+        CPPFLAGS=-I${BUILDEM_DIR}/include
+)
+
+# build single-precision
+ExternalProject_Add_Step(${libfftw_NAME} singlefloat-build
+    DEPENDEES singlefloat-configure
+    COMMAND cd ${libfftw_SRC_DIR}-build && ${BUILDEM_ENV_STRING} make
+)
+
+# install single-precision
+ExternalProject_Add_Step(${libfftw_NAME} singlefloat-install
+    DEPENDEES singlefloat-build
+    COMMAND cd ${libfftw_SRC_DIR}-build && ${BUILDEM_ENV_STRING} make install
 )
 
 set_target_properties(${libfftw_NAME} PROPERTIES EXCLUDE_FROM_ALL ON)
