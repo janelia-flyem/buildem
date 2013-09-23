e003b938cdac639ad877796cd4613b714caefb46
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2013-09-23 18:07:18 +0200

    don't use installed libatlas when WITH_ATLAS=OFF

--- openblas.cmake
+++ openblas.cmake
@@ -10,25 +10,23 @@ include (ExternalProject)
 include (ExternalSource)
 
 external_git_repo (openblas
-    0.2.5
+    0.2.8
     http://github.com/xianyi/OpenBLAS)
-    
-# NOTE: 'make' downloads and compiles lapack-3.4.2 on the fly and puts blas and lapack
-#       together into libopenblas.dll. numpy wants two separate libraries, so we copy the
-#       link library libopenblas.lib under the two names blas.lib and lapack.lib.
 
 message ("Installing ${openblas_NAME} into ilastik build area: ${BUILDEM_DIR} ...")
 ExternalProject_Add(${openblas_NAME}
 #    DEPENDS            
     PREFIX              ${BUILDEM_DIR}
     GIT_REPOSITORY      ${openblas_URL}
-    GIT_TAG             v0.2.5
+    GIT_TAG             v0.2.8
     UPDATE_COMMAND      ""
     PATCH_COMMAND       ""
     CONFIGURE_COMMAND   ""
     BUILD_COMMAND       make -j8 NO_AVX=1
     BUILD_IN_SOURCE     1
-    INSTALL_COMMAND     make PREFIX=${BUILDEM_DIR} install
+    INSTALL_COMMAND     make PREFIX=${BUILDEM_DIR} install &&
+                        ln -fs libopenblas.so ${BUILDEM_DIR}/lib/libblas.so &&
+                        ln -fs libopenblas.so ${BUILDEM_DIR}/lib/liblapack.so
 )
 
 set_target_properties(${openblas_NAME} PROPERTIES EXCLUDE_FROM_ALL ON)
