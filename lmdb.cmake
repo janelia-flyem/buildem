cd820b119ebabcf3ee37594a16a171f6169befa8
Bill Katz <billkatz@gmail.com>
2013-11-15 17:06:24 -0500

    Update Lightning MDB

--- lmdb.cmake
+++ lmdb.cmake
@@ -10,12 +10,11 @@ include (ExternalProject)
 include (ExternalSource)
 include (PkgConfig)
 include (BuildSupport)
-include (PatchSupport)
 
 external_source (lmdb
-    0.9.8
-    lmdb-0.9.8.tar.gz
-    834777d07442318e6a8a08b85c00833a)
+    0.9.10
+    lmdb-0.9.10.tar.gz
+    313e5015517e7183d5e85ba72293719c)
 
 message ("Installing ${lmdb_NAME} into FlyEM build area: ${BUILDEM_DIR} ...")
 ExternalProject_Add(${lmdb_NAME}
@@ -28,7 +27,7 @@ ExternalProject_Add(${lmdb_NAME}
     BUILD_IN_SOURCE     1
     BUILD_COMMAND       ${BUILDEM_ENV_STRING} make
     TEST_COMMAND        ${BUILDEM_ENV_STRING} make test
-    INSTALL_COMMAND     ${BUILDEM_ENV_STRING} make install BUILDEM_DIR=${BUILDEM_DIR}
+    INSTALL_COMMAND     ${BUILDEM_ENV_STRING} make install prefix=${BUILDEM_DIR} -i
 )
 
 set_target_properties(${lmdb_NAME} PROPERTIES EXCLUDE_FROM_ALL ON)
