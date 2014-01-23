f88fa60a0a3c1ad75febefba0599ec408162bbb1
Stuart Berg <bergs@janelia.hhmi.org>
2014-01-23 11:38:39 -0500

    ilastik.cmake: Fixed misformed shell command when ILASTIK_VERSION=master

--- ilastik.cmake
+++ ilastik.cmake
@@ -47,7 +47,7 @@ set(lazyflow_SRC_DIR "${ilastik_SRC_DIR}/lazyflow")
 
 if("${ILASTIK_VERSION}" STREQUAL "master")
 
-    set(ILASTIK_UPDATE_COMMAND git checkout master && git pull && git submodule update --init --recursive
+    set(ILASTIK_UPDATE_COMMAND git checkout master && git pull && git submodule update --init --recursive &&
                                cd lazyflow && git checkout master && git pull && git submodule update && cd .. &&
                                cd volumina && git checkout master && git pull && cd .. &&
                                cd ilastik && git checkout master && git pull && cd ..)
