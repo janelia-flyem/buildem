8e18fd15927821aff52cfef5763119f0cf86839a
Stuart Berg <bergs@janelia.hhmi.org>
2014-01-28 08:51:15 -0500

    cplex-shared.cmake: - Force cplex to be relocated, even if it was found previously. - Ensure we always use the STATIC library as input, even if a .so already exists.

--- cplex-shared.cmake
+++ cplex-shared.cmake
@@ -15,6 +15,12 @@ CMAKE_MINIMUM_REQUIRED(VERSION 2.8)
 # g++ -fpic -shared -Wl,-all_load libconcert.a -Wl,-noall_load -o libconcert.dylib
 # g++ -fpic -shared -Wl,-all_load libilocplex.a -Wl,-noall_load -L. -L../../../../concert/lib/x86-64_osx/static_pic -lcplex -lconcert -o libilocplex.dylib
 
+
+# Clear previously found location so we force it to be found again.
+set(CPLEX_LIBRARY CPLEX_LIBRARY-NOTFOUND)
+set(CPLEX_CONCERT_LIBRARY CPLEX_CONCERT_LIBRARY-NOTFOUND)
+set(CPLEX_ILOCPLEX_LIBRARY CPLEX_ILOCPLEX_LIBRARY-NOTFOUND)
+
 # Find the CPLEX static libraries (must already be installed somewhere)
 find_package( Cplex REQUIRED )
 
@@ -31,8 +37,8 @@ endif()
 ##
 ## -- Create a shared library from libcplex.a
 ##
-set( CPLEX_LIBRARY_STATIC ${CPLEX_LIBRARY} )
-get_filename_component(CPLEX_LIB_DIR "${CPLEX_LIBRARY_STATIC}" PATH)
+get_filename_component( CPLEX_LIBRARY_DIR ${CPLEX_LIBRARY} PATH )
+set( CPLEX_LIBRARY_STATIC ${CPLEX_LIBRARY_DIR}/libcplex.a )
 
 # Create an empty cpp file to use as a stand-in for cpp sources in the library
 set (EMPTY_CPP ${CMAKE_CURRENT_BINARY_DIR}/empty.cpp)
@@ -43,7 +49,7 @@ target_link_libraries( cplex-shared ${SHARED_CPLEX_LINK_FLAGS_1} ${CPLEX_LIBRARY
 set_target_properties( cplex-shared PROPERTIES 
                        COMPILE_FLAGS ${SHARED_CPLEX_COMPILE_FLAGS}
                        OUTPUT_NAME cplex
-                       LIBRARY_OUTPUT_DIRECTORY ${CPLEX_LIB_DIR} )
+                       LIBRARY_OUTPUT_DIRECTORY ${CPLEX_LIBRARY_DIR} )
 
 get_property(CPLEX_LIB_LOCATION TARGET cplex-shared PROPERTY LOCATION)
 
@@ -51,23 +57,23 @@ get_property(CPLEX_LIB_LOCATION TARGET cplex-shared PROPERTY LOCATION)
 ##
 ## -- Create a shared library from libconcert.a
 ##
-set( CONCERT_LIBRARY_STATIC ${CPLEX_CONCERT_LIBRARY} )
-get_filename_component(CONCERT_LIB_DIR "${CONCERT_LIBRARY_STATIC}" PATH)
+get_filename_component( CONCERT_LIBRARY_DIR ${CPLEX_CONCERT_LIBRARY} PATH )
+set( CONCERT_LIBRARY_STATIC ${CONCERT_LIBRARY_DIR}/libconcert.a )
 
 add_library( concert-shared SHARED ${EMPTY_CPP} )
 target_link_libraries( concert-shared ${SHARED_CPLEX_LINK_FLAGS_1} ${CONCERT_LIBRARY_STATIC} ${SHARED_CPLEX_LINK_FLAGS_2} )
 set_target_properties( concert-shared PROPERTIES 
                        COMPILE_FLAGS ${SHARED_CPLEX_COMPILE_FLAGS}
                        OUTPUT_NAME concert
-                       LIBRARY_OUTPUT_DIRECTORY ${CONCERT_LIB_DIR} )
+                       LIBRARY_OUTPUT_DIRECTORY ${CONCERT_LIBRARY_DIR} )
 
 get_property(CONCERT_LIB_LOCATION TARGET concert-shared PROPERTY LOCATION)
 
 ##
 ## -- Create a shared library from libilocplex.a, which depends on the above two libraries.
 ##
-set( ILOCPLEX_LIBRARY_STATIC ${CPLEX_ILOCPLEX_LIBRARY} )
-get_filename_component(ILOCPLEX_LIB_DIR "${ILOCPLEX_LIBRARY_STATIC}" PATH)
+get_filename_component( ILOCPLEX_LIBRARY_DIR ${CPLEX_ILOCPLEX_LIBRARY} PATH )
+set( ILOCPLEX_LIBRARY_STATIC ${ILOCPLEX_LIBRARY_DIR}/libilocplex.a )
 
 add_library( ilocplex-shared SHARED ${EMPTY_CPP} )
 add_dependencies( ilocplex-shared cplex-shared concert-shared )
@@ -75,4 +81,4 @@ target_link_libraries( ilocplex-shared ${SHARED_CPLEX_LINK_FLAGS_1} ${ILOCPLEX_L
 set_target_properties( ilocplex-shared PROPERTIES 
                        COMPILE_FLAGS ${SHARED_CPLEX_COMPILE_FLAGS}
                        OUTPUT_NAME ilocplex
-                       LIBRARY_OUTPUT_DIRECTORY ${ILOCPLEX_LIB_DIR} )
+                       LIBRARY_OUTPUT_DIRECTORY ${ILOCPLEX_LIBRARY_DIR} )
