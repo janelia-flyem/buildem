1a091f0bbb7c614e7fd76701523d2276aabddff6
Stuart Berg <bergs@janelia.hhmi.org>
2014-03-05 11:30:12 -0500

    pyqt4.cmake: Updated to 4.10.3

--- pyqt4.cmake
+++ pyqt4.cmake
@@ -19,25 +19,25 @@ message ("Installing ${pyqt4_NAME} into FlyEM build area: ${BUILDEM_DIR} ...")
 
 if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
     external_source (pyqt4
-        4.9.5
-        PyQt-mac-gpl-4.9.5.tar.gz
-        855f9d9e10821c0f79a7dc956a9a14ec    
-        http://iweb.dl.sourceforge.net/project/pyqt/PyQt4/PyQt-4.9.5)
+        4.10.3
+        PyQt-mac-gpl-4.10.3.tar.gz
+        4dfb646512dd479cfa7518605eda1f32    
+        http://iweb.dl.sourceforge.net/project/pyqt/PyQt4/PyQt-4.10.3)
 
     set (EXTRA_PYQT4_CONFIG_FLAGS
         --use-arch=x86_64)
 elseif (${WIN32})
     external_source (pyqt4
-        4.9.5
-        PyQt-win-gpl-4.9.5.zip
-        0ebc1b0c27b49c22492a3145f5f1ee47
-        http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.9.5)
+        4.10.3
+        PyQt-win-gpl-4.10.3.zip
+        14f09b0ed9f04daf8f19f680d3b90e8b
+        http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.10.3)
 elseif (${UNIX})
     external_source (pyqt4
-        4.9.5
-        PyQt-x11-gpl-4.9.5.tar.gz
-        e4cdd6619c63655f7510efb4df8462fb    
-        http://iweb.dl.sourceforge.net/project/pyqt/PyQt4/PyQt-4.9.5)
+        4.10.3
+        PyQt-x11-gpl-4.10.3.tar.gz
+        8b13d2ab64e4d2fd0037b81b7e78c15c    
+        http://iweb.dl.sourceforge.net/project/pyqt/PyQt4/PyQt-4.10.3)
 else ()
     message (FATAL_ERROR "ERROR: Cannot detect valid system in pyqt4 cmake file")
 endif()
@@ -48,10 +48,10 @@ ExternalProject_Add(${pyqt4_NAME}
     URL                 ${pyqt4_URL}
     URL_MD5             ${pyqt4_MD5}
     UPDATE_COMMAND      ""
-    PATCH_COMMAND       ${BUILDEM_ENV_STRING} ${PATCH_EXE}
-        ${pyqt4_SRC_DIR}/configure.py ${PATCH_DIR}/pyqt4.patch # For some reason, the configure script wants to build pyqt phonon support even if qt was built without it.  This patch simply comments out phonon support.
+    PATCH_COMMAND       ""
     CONFIGURE_COMMAND   ${BUILDEM_ENV_STRING} ${PYTHON_EXE} ${pyqt4_SRC_DIR}/configure.py 
         --confirm-license
+        --verbose
         -q "${BUILDEM_DIR}/bin/qmake"
         ${EXTRA_PYQT4_CONFIG_FLAGS}
     BUILD_COMMAND       ${BUILDEM_ENV_STRING} $(MAKE)
