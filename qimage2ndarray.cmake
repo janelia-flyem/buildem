1bbf43bd68ec6565cebc847a37fc5bbd3c3f3c3e
Stuart Berg <bergs@janelia.hhmi.org>
2014-03-05 11:30:44 -0500

    qimage2ndarray.cmake: - Updated to v1.3, which is required for compatibility with newer versions of sip. - Pull from github, not the PyPi tarball, which is apparently malformed.

--- qimage2ndarray.cmake
+++ qimage2ndarray.cmake
@@ -14,29 +14,22 @@ include (BuildSupport)
 include (numpy)
 include (pyqt4)
 
-external_source (qimage2ndarray
-    1.0
-    qimage2ndarray-1.0.tar.gz
-    1f59c1c42395709a58c43ed74a866375    
-    http://kogs-www.informatik.uni-hamburg.de/~meine/software/qimage2ndarray/dist)
-
-if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-    # This patch does no harm on non-mac builds, but there's no need to apply it on linux.
-    set (qimage2ndarray_PATCH ${BUILDEM_ENV_STRING} ${PATCH_EXE}
-        ${qimage2ndarray_SRC_DIR}/setup.py ${PATCH_DIR}/qimage2ndarray.patch )
-else()
-    set (qimage2ndarray_PATCH "")
-endif()
+# The 1.3 tarball from PyPi seems malformed: 
+#  it doesn't have an /include directory!
+# Instead, we pull directly from github.
+external_git_repo (qimage2ndarray
+    release-1.3
+    https://github.com/hmeine/qimage2ndarray)
 
 message ("Installing ${qimage2ndarray_NAME} into FlyEM build area: ${BUILDEM_DIR} ...")
 
 ExternalProject_Add(${qimage2ndarray_NAME}
     DEPENDS             ${numpy_NAME} ${pyqt4_NAME}
     PREFIX              ${BUILDEM_DIR}
-    URL                 ${qimage2ndarray_URL}
-    URL_MD5             ${qimage2ndarray_MD5}
+    GIT_REPOSITORY      ${qimage2ndarray_URL}
+    GIT_TAG             ${qimage2ndarray_TAG}
     UPDATE_COMMAND      ""
-    PATCH_COMMAND       ${qimage2ndarray_PATCH}
+    PATCH_COMMAND       ""
     LIST_SEPARATOR      ^^
     CONFIGURE_COMMAND   ""
     BUILD_COMMAND       ${BUILDEM_ENV_STRING} ${PYTHON_EXE} ${qimage2ndarray_SRC_DIR}/setup.py build
