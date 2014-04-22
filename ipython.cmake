376307c47bd5800ec4bf058e3b8895d92eab65cd
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-04-22 16:03:12 -0400

    ipython: Added iPython and dependencies. This includes install readline in a compatiable way for Mac and Windows. Uses the 1.2.1 ( last of version 1 ).

---
+++ ipython.cmake
@@ -0,0 +1,44 @@
+# Standard of Python interactive programming.
+# Required by Spyder.
+
+if (NOT ipython_NAME)
+
+CMAKE_MINIMUM_REQUIRED(VERSION 2.8)
+
+include (ExternalProject)
+include (ExternalSource)
+include (BuildSupport)
+
+
+include (python)
+include (pyqt4)
+include (sphinxcontrib-napoleon)  # For sphinx extension. Doesn't necessarily need napolean, but it is nice to have and is quick to install.
+include (readline)
+include (pyzmq)
+include (nose)
+include (pygments)
+include (tornado)
+include (jinja)
+
+
+external_git_repo(ipython
+	rel-1.2.1 #9c4f59ddd54d797072d28d2d5549bc5ea234216d
+	https://github.com/ipython/ipython/)
+
+message ("Installing ${ipython_NAME} into FlyEM build area: ${BUILDEM_DIR} ...")
+ExternalProject_Add(${ipython_NAME}
+    DEPENDS             ${python_NAME} ${sphinxcontrib-napoleon_NAME} ${readline_NAME} ${pyzmq_NAME} ${nose_NAME} ${pygments_NAME} ${tornado_NAME} ${jinja_NAME}
+    PREFIX              ${BUILDEM_DIR}
+    GIT_REPOSITORY	${ipython_URL}
+    GIT_TAG             ${ipython_TAG}
+    UPDATE_COMMAND      ""
+    PATCH_COMMAND       ""
+    CONFIGURE_COMMAND   ""
+    BUILD_COMMAND       ${BUILDEM_ENV_STRING} ${PYTHON_EXE} setup.py build
+    BUILD_IN_SOURCE     1
+    INSTALL_COMMAND     ${BUILDEM_ENV_STRING} ${PYTHON_EXE} setup.py install
+)
+
+set_target_properties(${ipython_NAME} PROPERTIES EXCLUDE_FROM_ALL ON)
+
+endif (NOT ipython_NAME)
