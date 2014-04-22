b74d53fadc91d764bab68fa0cb5b9606e62ecf48
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-04-22 17:12:02 -0400

    scipy-stack: Provides a custom target for installing the full SciPy stack. Includes everything in the SciPy stack and in a few cases a little extra. Does not include Spyder.

---
+++ scipy-stack.cmake
@@ -0,0 +1,32 @@
+# Pseudo-package for installing the full SciPy stack.
+
+if (NOT scipy-stack_NAME)
+
+CMAKE_MINIMUM_REQUIRED(VERSION 2.8)
+
+scipy-stack_NAME = "scipy-stack"
+
+include (ExternalProject)
+include (ExternalSource)
+include (BuildSupport)
+
+
+include (python)
+include (sphinxcontrib-napoleon)    # Nice Sphinx docs
+include (pyqt4)
+include (numpy)
+include (scipy)
+include (matplotlib)
+include (statsmodels)  # Includes pandas as a dependency
+include (scikit-image)
+include (scikit-learn)
+include (sympy)         # Pulls in gmpy2
+include (ipython)
+include (nose)
+
+
+add_custom_target(scipy-stack
+    DEPENDS             ${python_NAME} ${sphinxcontrib-napoleon} ${pyqt4_NAME} ${numpy_NAME} ${scipy_NAME} ${matplotlib_NAME} ${statsmodels_NAME} ${scikit-image_NAME} ${scikit-learn_NAME} ${sympy_NAME} ${ipython_NAME} ${nose_NAME}
+)
+
+endif (NOT scipy-stack_NAME)
