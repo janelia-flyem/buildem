802a976a001054a846e239064e83c93ac86a7b2b
martinsch <martin.schiegg@iwr.uni-heidelberg.de>
2014-07-11 00:36:56 +0200

    Update pgmlink SHA

--- pgmlink.cmake
+++ pgmlink.cmake
@@ -37,7 +37,7 @@ else()
     include (cplex-shared)
 
     external_git_repo (pgmlink
-        9c5e87503bcbf453e14acf3cafdafa9a76c69921
+        7a0c8d4f54e9a4066991829fb52b411a02914666
         https://github.com/martinsch/pgmlink)
 
     message ("Installing ${pgmlink_NAME} into FlyEM build aread: ${BUILDEM_DIR} ...")
