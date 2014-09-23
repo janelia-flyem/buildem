30843d18b856a43fbf0a38e307bd276b8a93f813
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-09-23 09:12:59 -0400

    boost.cmake: Upgraded version of Boost.

--- boost.cmake
+++ boost.cmake
@@ -17,10 +17,10 @@ set (boost_INCLUDE_DIR  ${BUILDEM_INCLUDE_DIR}/boost)
 include_directories (${boost_INCLUDE_DIR})
 
 external_source (boost
-    1_55_0
-    boost_1_55_0.tar.gz
-    93780777cfbf999a600f62883bd54b17 
-    http://sourceforge.net/projects/boost/files/boost/1.55.0/)
+    1_56_0
+    boost_1_56_0.tar.bz2
+    a744cf167b05d72335f27c88115f211d
+    http://hivelocity.dl.sourceforge.net/project/boost/boost/1.56.0)
 
 set (boost_LIBS ${BUILDEM_LIB_DIR}/libboost_thread.${BUILDEM_PLATFORM_DYLIB_EXTENSION} 
                 ${BUILDEM_LIB_DIR}/libboost_system.${BUILDEM_PLATFORM_DYLIB_EXTENSION}
