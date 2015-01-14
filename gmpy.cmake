45471404bdff27e2bea55f377a25091d04ca25b6
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-01-14 18:28:19 -0500

    gmpy.cmake: Turn off FORCE as the archive is already in the cache.

--- gmpy.cmake
+++ gmpy.cmake
@@ -22,8 +22,7 @@ external_source (gmpy
     2.0.3
     gmpy2-2.0.3.zip
     63f367b4dceb20dcd72c143e7c9a8632
-    http://pypi.python.org/packages/source/g/gmpy2/
-    FORCE)
+    http://pypi.python.org/packages/source/g/gmpy2/)
 
 
 message ("Installing ${gmpy_NAME} into FlyEM build area: ${BUILDEM_DIR} ...")
