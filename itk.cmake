c6e1ae28b25d971f0f840a93ae776282db0f8338
Stuart Berg <bergs@janelia.hhmi.org>
2015-03-11 00:40:26 -0400

    itk: Only build the minimal set of modules required by the iiboost workflow in ilastik.

--- itk.cmake
+++ itk.cmake
@@ -44,33 +44,38 @@ ExternalProject_Add(${itk_NAME}
         -DBUILD_SHARED_LIBS:BOOL=ON
         -DCMAKE_CXX_FLAGS=-I${BUILDEM_DIR}/include
         -DCMAKE_C_FLAGS=-I${BUILDEM_DIR}/include
+        -DITK_BUILD_DEFAULT_MODULES=0
+        -DBUILD_EXAMPLES=0
+        -DITKGroup_Core=1
+        -DModule_ITKConvolution=1
+        -DModule_ITKEigen=1
     
-    # hdf5
-    -DITK_USE_SYSTEM_HDF5=ON
-    -DHDF5_C_LIBRARY=${BUILDEM_LIB_DIR}/libhdf5.so.1.8.9
-    -DHDF5_DIR=${BUILDEM_DIR}/share/cmake/hdf5
-    # libpng
-    -DITK_USE_SYSTEM_PNG=ON
-    -DPNG_PNG_INCLUDE_DIR=${BUILDEM_INCLUDE_DIR} # PNG_PNG looks wrong, but that's what the variable is named.
-    -DPNG_LIBRARY=${BUILDEM_LIB_DIR}/libpng.${BUILDEM_PLATFORM_DYLIB_EXTENSION}
-    # libjpeg
-    -DITK_USE_SYSTEM_JPEG=ON
-    -DJPEG_INCLUDE_DIR=${BUILDEM_INCLUDE_DIR}
-    -DJPEG_LIBRARY=${BUILDEM_LIB_DIR}/libjpeg.${BUILDEM_PLATFORM_DYLIB_EXTENSION}
-    # libtiff
-    -DITK_USE_SYSTEM_TIFF=ON
-    -DTIFF_INCLUDE_DIR=${BUILDEM_INCLUDE_DIR}
-    -DTIFF_LIBRARY=${BUILDEM_LIB_DIR}/libtiff.${BUILDEM_PLATFORM_DYLIB_EXTENSION}    
-    # zlib
-    -DITK_USE_SYSTEM_ZLIB=ON
-    -DZLIB_INCLUDE_DIR=${BUILDEM_INCLUDE_DIR}
-    -DZLIB_LIBRARY=${BUILDEM_LIB_DIR}/libz.${BUILDEM_PLATFORM_DYLIB_EXTENSION}
+	# hdf5
+	-DITK_USE_SYSTEM_HDF5=ON
+	-DHDF5_C_LIBRARY=${BUILDEM_LIB_DIR}/libhdf5.so.1.8.9
+	-DHDF5_DIR=${BUILDEM_DIR}/share/cmake/hdf5
+	# libpng
+	-DITK_USE_SYSTEM_PNG=ON
+	-DPNG_PNG_INCLUDE_DIR=${BUILDEM_INCLUDE_DIR} # PNG_PNG looks wrong, but that's what the variable is named.
+	-DPNG_LIBRARY=${BUILDEM_LIB_DIR}/libpng.${BUILDEM_PLATFORM_DYLIB_EXTENSION}
+	# libjpeg
+	-DITK_USE_SYSTEM_JPEG=ON
+	-DJPEG_INCLUDE_DIR=${BUILDEM_INCLUDE_DIR}
+	-DJPEG_LIBRARY=${BUILDEM_LIB_DIR}/libjpeg.${BUILDEM_PLATFORM_DYLIB_EXTENSION}
+	# libtiff
+	-DITK_USE_SYSTEM_TIFF=ON
+	-DTIFF_INCLUDE_DIR=${BUILDEM_INCLUDE_DIR}
+	-DTIFF_LIBRARY=${BUILDEM_LIB_DIR}/libtiff.${BUILDEM_PLATFORM_DYLIB_EXTENSION}    
+	# zlib
+	-DITK_USE_SYSTEM_ZLIB=ON
+	-DZLIB_INCLUDE_DIR=${BUILDEM_INCLUDE_DIR}
+	-DZLIB_LIBRARY=${BUILDEM_LIB_DIR}/libz.${BUILDEM_PLATFORM_DYLIB_EXTENSION}
     
     
-	# We want itk to be built in parallel if possible.
-	# Therefore we use $(MAKE) instead of 'make', which somehow enables sub-make files to use the jobserver correctly.
-	# See: http://stackoverflow.com/questions/2942465/cmake-and-parallel-building-with-make-jn
-	# And: http://www.cmake.org/pipermail/cmake/2011-April/043720.html
+    # We want itk to be built in parallel if possible.
+    # Therefore we use $(MAKE) instead of 'make', which somehow enables sub-make files to use the jobserver correctly.
+    # See: http://stackoverflow.com/questions/2942465/cmake-and-parallel-building-with-make-jn
+    # And: http://www.cmake.org/pipermail/cmake/2011-April/043720.html
     BUILD_COMMAND       ${BUILDEM_ENV_STRING} $(MAKE)
     #TEST_COMMAND        ${BUILDEM_ENV_STRING} $(MAKE) check
     INSTALL_COMMAND     ${BUILDEM_ENV_STRING} $(MAKE) install
