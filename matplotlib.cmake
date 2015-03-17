1619af4b5b9c13993d998062d94e6db54ad17dde
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-17 13:33:14 -0400

    matplotlib.cmake: Fix missing _NAME.

--- matplotlib.cmake
+++ matplotlib.cmake
@@ -36,7 +36,7 @@ external_git_repo(matplotlib
 
 message ("Installing ${matplotlib_NAME} into FlyEM build area: ${BUILDEM_DIR} ...")
 ExternalProject_Add(${matplotlib_NAME}
-    DEPENDS             ${python_NAME} ${pyqt4} ${six_NAME} ${setuptools_NAME} ${pytz_NAME} ${python-dateutil_NAME} ${pyparsing_NAME} ${numpy_NAME} ${libpng_NAME} ${ffmpeg_NAME} ${freetype2_NAME} ${nose_NAME} ${mock_NAME} ${tornado_NAME}
+    DEPENDS             ${python_NAME} ${pyqt4_NAME} ${six_NAME} ${setuptools_NAME} ${pytz_NAME} ${python-dateutil_NAME} ${pyparsing_NAME} ${numpy_NAME} ${libpng_NAME} ${ffmpeg_NAME} ${freetype2_NAME} ${nose_NAME} ${mock_NAME} ${tornado_NAME}
     PREFIX              ${BUILDEM_DIR}
     GIT_REPOSITORY      ${matplotlib_URL}
     GIT_TAG             ${matplotlib_TAG}
